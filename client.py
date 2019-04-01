838d39ad39dbf93b15adc977180d664f32b44ed2
Kim <kimberly.lennox@duke.edu>
2019-04-01 15:55:16 -0400

    Database can now save multiple heart rates. Timestamps have been added

--- client.py
+++ client.py
@@ -4,22 +4,30 @@ r = requests.get("http://127.0.0.1:5000")
 print(r.text)
 
 
-# patient = {
-#           "patient_id": "1",
-#           "attending_email": "lennox_kimberly@yahoo.com",
-#           "user_age": 50,
-#           }
+patient = {
+           "patient_id": "1",
+           "attending_email": "lennox_kimberly@yahoo.com",
+           "user_age": 50,
+           }
 
-#  r = requests.post("http://127.0.0.1:5000/api/new_patient", json=patient)
-#  print(r.json())
+r = requests.post("http://127.0.0.1:5000/api/new_patient", json=patient)
+print(r.json())
 
 HR = {
      "patient_id": "1",
      "heart_rate": 100
      }
 r3 = requests.post("http://127.0.0.1:5000/api/heart_rate", json=HR)
+print(r3.text)
+
+HR = {
+     "patient_id": "1",
+     "heart_rate": 90
+     }
+
+r3 = requests.post("http://127.0.0.1:5000/api/heart_rate", json=HR)
 print(r3.json())
 
-
-r2 = requests.get("http://127.0.0.1:5000/api/heart_rate/0000")
+r2 = requests.get("http://127.0.0.1:5000/api/heart_rate/1")
+print("Retrieved HR")
 print(r2.json())
