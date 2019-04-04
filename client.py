e62ff88ea1088a8713c7330d600f6b07586b59fc
Kim <kimberly.lennox@duke.edu>
2019-04-04 14:57:58 -0400

    Added interval average and status checks

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import requests
+import time
+import datetime
 
 r = requests.get("http://127.0.0.1:5000")
 print(r.text)
@@ -25,10 +27,13 @@ HR = {
      "heart_rate": 90
      }
 
-
+time.sleep(5)
 r3 = requests.post("http://127.0.0.1:5000/api/heart_rate", json=HR)
 print(r3.text)
-
+TJ = r3.text
+TJ = TJ.rstrip("\n")
+TJ = TJ.replace('"', '')
+T = datetime.datetime.strptime(TJ, "%a, %d %b %Y %H:%M:%S %Z")
 HR = {
      "patient_id": "1",
      "heart_rate": 100
@@ -48,3 +53,11 @@ print(r4.json())
 
 r5 = requests.get("http://127.0.0.1:5000/api/heart_rate/average/1")
 print(r5.json())
+HR = {
+     "patient_id": "1",
+     "heart_rate_average_since": TJ
+     }
+
+r6 = requests.get("http://127.0.0.1:5000/api/heart_rate/interval_average",
+                  json=HR)
+print(r6.json())
--- server.py
+++ server.py
@@ -3,7 +3,8 @@ from pymodm import connect
 from pymodm import MongoModel, fields
 import numpy as np
 import datetime
-
+import math
+import logging
 app = Flask(__name__)
 connect("mongodb://localhost:27017/SentinelServer")
 
@@ -35,12 +36,83 @@ def HRStatus(HR):
 
 
 def CalcAverage(HR):
+    """
+    Calculates average heart rates from a list
+    Args: HR, list of heart rates
+    Returns: avg, average value
+    """
     if HR == []:
         avg = 0
     else:
         vec = np.array(HR)
         avg = np.mean(HR)
     return avg
+
+
+def GetIndex(T, time):
+    """
+    Finds all timestamp values after a timestamp specified
+    by the user, assuming timestamps are in order
+    Args:
+        T: vector of timestamps
+        time: timestamp reference
+    Returns:
+        idx_min: minimum index of timestamp after the reference time
+    """
+    T_array = np.array(T, dtype='datetime64')
+    idx = np.where(T_array >= time)
+    idx_min = (idx[0])
+    idx_min = idx_min[0]
+    return idx_min
+
+
+def CheckFormat(r):
+    try:
+        t = str(r["patient_id"])
+        a = str(r["attending_email"])
+        u = int(r["user_age"])
+        u_test = u-1
+        x = "Patient saved successfully"
+    except KeyError:
+        x = "Incorrect format: please try again"
+    except TypeError:
+        x = "Incorrect format: please try again"
+    except ValueError:
+        x = "Incorrect format: please try again"
+    return x
+
+
+def CheckHRData(r):
+    x = 0
+    try:
+        t = str(r["patient_id"])
+        m = (r["heart_rate"])
+        n = math.sqrt(m)  # Check if value is positive
+        m = m/3
+        if m < 0:
+            x = "Incorrect format: please try again"
+    except KeyError:
+        x = "Incorrect format: please try again"
+    except TypeError:
+        x = "Incorrect format: please try again"
+    except ValueError:
+        x = "Incorrect format: please try again"
+    return x
+
+
+def CheckIntervalData(r):
+    x = 0
+    try:
+        ID = str(r["patient_id"])
+        time = str(r["heart_rate_average_since"])
+        time = datetime.datetime.strptime(time, "%a, %d %b %Y %H:%M:%S %Z")
+    except KeyError:
+        x = "Incorrect format: please try again"
+    except TypeError:
+        x = "Incorrect format: please try again"
+    except ValueError:
+        x = "Incorrect format: please try again"
+    return x
 
 
 @app.route("/", methods=["GET"])
@@ -59,12 +131,16 @@ def newpatient():
     JSON format
     """
     r = request.get_json()
-    ID = r["patient_id"]
-    a_email = r["attending_email"]
-    age = r["user_age"]
-    u = User(ID, email=a_email, user_age=age)
-    u.save()
-    info = "Patient saved successfully"
+    x = CheckFormat(r)
+    if x == "Incorrect format: please try again":
+        info = "Incorrect input patient format; please try again"
+    else:
+        ID = str(r["patient_id"])
+        a_email = r["attending_email"]
+        age = r["user_age"]
+        u = User(ID, email=a_email, user_age=age)
+        u.save()
+        info = x
     return jsonify(info)
 
 
@@ -112,22 +188,27 @@ def sendHR():
     Timestamp of request
     """
     r = request.get_json()
-    patient_id = str(r["patient_id"])
-    HR_ind = r["heart_rate"]
-    T_ind = datetime.datetime.now()
-    user = User.objects.raw({"_id": patient_id}).first()
-    HR = user.heart_rate
-    if user.heart_rate is None:
-        HR = HR_ind
-        T = T_ind
+    x = CheckHRData(r)
+    if x == "Incorrect format: please try again":
+        info = "Incorrect HR Data format; please try again"
     else:
-        T = user.timestamp
-        HR.append(HR_ind)
-        T.append(T_ind)
-    user.heart_rate = HR
-    user.timestamp = T
-    user.save()
-    return jsonify(T_ind)
+        patient_id = str(r["patient_id"])
+        HR_ind = r["heart_rate"]
+        T_ind = datetime.datetime.now()
+        user = User.objects.raw({"_id": patient_id}).first()
+        HR = user.heart_rate
+        info = T_ind
+        if user.heart_rate is None:
+            HR = HR_ind
+            T = T_ind
+        else:
+            T = user.timestamp
+            HR.append(HR_ind)
+            T.append(T_ind)
+        user.heart_rate = HR
+        user.timestamp = T
+        user.save()
+    return jsonify(info)
 
 
 @app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
@@ -137,4 +218,29 @@ def GetAverage(patient_id):
     avg = CalcAverage(user.heart_rate)
     out = {"Patient ID": patient_id,
            "HR Average": avg}
+    return jsonify(out)
+
+
+@app.route("/api/heart_rate/interval_average", methods=["GET"])
+def IntervalAverage():
+    r = request.get_json()
+    x = CheckIntervalData(r)
+    if x == "Incorrect format: please try again":
+        out = "Incorrect interval average format; please try again"
+    else:
+        time = r["heart_rate_average_since"]
+        time = datetime.datetime.strptime(time, "%a, %d %b %Y %H:%M:%S %Z")
+        patient_id = str(r["patient_id"])
+        user = User.objects.raw({"_id": patient_id}).first()
+        HRmat = user.heart_rate
+        T = user.timestamp
+        if user.heart_rate == []:
+            out = "None"
+        else:
+            idx = GetIndex(T, time)
+            if idx == []:
+                out = "None"
+            else:
+                HR = HRmat[idx:len(HRmat)]
+                out = CalcAverage(HR)
     return jsonify(out)
